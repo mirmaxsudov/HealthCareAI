335c10b1f1d2005fce145c1fd8ddaa3e40c55846
Abdurahmon <abdurahmonmirmaxsudov2804@gmail.com>
2024-07-22 16:25:14 +0500

    commit

---
+++ GateWayService/src/test/java/uz/abdurahmon/gatewayservice/GateWayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.abdurahmon.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
